aa0a062e72f0c3dea34981f6fb813bf94b2ed094
Nhat-Hao-05 <2311556768@nttu.edu.vn>
2026-08-13 12:39:21 +0700

    Initial commit

--- MyEStore/obj/Debug/net8.0/MyEStore.AssemblyInfo.cs
+++ MyEStore/obj/Debug/net8.0/MyEStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyEStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbc25bf6ebe17392957c760656cb7dba69632b60")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyEStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyEStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
